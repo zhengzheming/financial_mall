95766f69e78351e584b426565a1b824ec45160b6
jackie-zheng <zheming.zheng@zygjny.com>
2018-11-09 14:33:18 +0800

    抽离页面初始化代码 #000

--- src/views/page1/main.js
+++ src/views/page1/main.js
@@ -1,51 +1,6 @@
-import Vue from "vue";
-import App from "../App.vue";
+import initPage from "@/views/initPage";
+import requestWrap from "@/api/mall/request";
 import router from "./router";
 import store from "./store";
-import Vant from "vant";
-import VeeValidate from "vee-validate";
-import zh from "@/services/verify/zh";
-import "@/assets/scss/common.scss";
-import "@/assets/css/bank_small_logo.css";
-import "vant/lib/vant-css/index.css";
-import "@/assets/scss/overide.scss";
-import initInterceptor from "@/services/interceptors";
-import request from "@/api/mall/request";
-import "@/components";
-import "@/filter";
-import api from "@/api";
-import extendValidator from "@/services/verify";
-import { Toast } from "vant";
 
-Vue.use(Vant);
-Vue.use(VeeValidate);
-VeeValidate.Validator.localize("zh", zh);
-extendValidator(VeeValidate.Validator);
-initInterceptor(request(), router);
-Vue.config.productionTip = false;
-Object.defineProperty(Vue.prototype, "$log", { value: window.console.log });
-Object.defineProperty(Vue.prototype, "$apiService", { value: api.mall });
-window.$apiService = api.mall;
-
-// 初始化操作
-router.beforeEach((to, from, next) => {
-  const loginInfo = localStorage.getItem("login");
-  if (!loginInfo && to.name !== "login") {
-    Toast("请先登录");
-    next({ name: "login" });
-  } else {
-    next();
-  }
-});
-api.mall.getUserInfo().then(res => {
-  new Vue({
-    router,
-    store,
-    render: h => h(App)
-  }).$mount("#app");
-  if (res.code == 0) {
-    store.dispatch("userinfo:get", res.data);
-  } else {
-    router.replace({ name: "login" });
-  }
-});
+initPage("mall", requestWrap(), router, store);
--- src/views/page2/main.js
+++ src/views/page2/main.js
@@ -1,51 +1,6 @@
-import Vue from "vue";
-import App from "../App.vue";
+import initPage from "@/views/initPage";
+import requestWrap from "@/api/market/request";
 import router from "./router";
 import store from "./store";
-import Vant from "vant";
-import VeeValidate from "vee-validate";
-import zh from "@/services/verify/zh";
-import "@/assets/scss/common.scss";
-import "@/assets/css/bank_small_logo.css";
-import "vant/lib/vant-css/index.css";
-import "@/assets/scss/overide.scss";
-import "@/services/interceptors";
-import "@/components";
-import "@/filter";
-import api from "@/api";
-import extendValidator from "@/services/verify";
-import { Toast } from "vant";
-import initInterceptor from "@/services/interceptors";
-import request from "@/api/market/request";
 
-Vue.use(Vant);
-
-Vue.use(VeeValidate);
-VeeValidate.Validator.localize("zh", zh);
-extendValidator(VeeValidate.Validator);
-initInterceptor(request(), router);
-Vue.config.productionTip = false;
-Object.defineProperty(Vue.prototype, "$log", { value: window.console.log });
-Object.defineProperty(Vue.prototype, "$apiService", { value: api.market });
-window.$apiService = api.market;
-router.beforeEach((to, from, next) => {
-  if (!store.state.phone && to.name !== "login") {
-    Toast("请先登录");
-    next({ name: "login" });
-  } else {
-    next();
-  }
-});
-api.market.getUserInfo().then(res => {
-  // 登录态验证
-  new Vue({
-    router,
-    store,
-    render: h => h(App)
-  }).$mount("#app");
-  if (res.code == 0) {
-    store.dispatch("userinfo:get", res.data);
-  } else {
-    router.replace({ name: "login" });
-  }
-});
+initPage("market", requestWrap(), router, store);
